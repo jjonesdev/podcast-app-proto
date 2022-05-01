ef75b07572f39a4dd987e5f2610adf126b439969
Jordan <jt.github@fastmail.com>
2022-05-01 14:29:01 -0400

    Create prototype tab bar

---
+++ iOSPrototype/TabBarView.swift
@@ -0,0 +1,31 @@
+//
+//  TabBarView.swift
+//  iOSPrototype
+//
+//  Created by Jordan on 5/1/22.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+  var body: some View {
+    TabView {
+      Text("Library View")
+        .tabItem {
+          Image(systemName: "square.stack")
+          Text("Library")
+        }
+      Text("Search")
+        .tabItem {
+          Image(systemName: "magnifyingglass")
+          Text("Searh View")
+        }
+    }
+  }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+  static var previews: some View {
+    TabBarView()
+  }
+}
