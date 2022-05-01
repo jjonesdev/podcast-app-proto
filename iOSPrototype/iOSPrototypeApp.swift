481da0807021ee84d32b1ad35d25bec9464a1067
Jordan <jt.github@fastmail.com>
2022-05-01 14:18:12 -0400

    Remove `iOS` target and create a new SwiftUI `iOSPrototype` target

---
+++ iOSPrototype/iOSPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Jordan on 5/1/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
